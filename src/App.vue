0b353551a70e21b993f54b9707f12eb069b770e6
Alex <alex@q-83.com>
2021-05-25 21:44:36 +1000

    day 1 | 更新了 abnner，search location

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="docker">
-    <span
-      v-for="(item, idx) in dockerItems"
-      :key="idx"
-      class="docker__item"
-      :class="[item.isActive ? 'docker__item--active' : '']"
-    >
-      <i :class="[item.iconName]"></i>
-      <span>{{ item.label }}</span>
-    </span>
+  <div>
+    <div class="wrapper">
+      <div class="location">
+        <i class="location__icon el-icon-location-outline" />
+        <span> this is my location</span>
+        <i class="location__icon el-icon-message-solid"></i>
+        <div></div>
+      </div>
+      <el-input
+        v-model="searchInput"
+        prefix-icon="el-icon-search"
+        placeholder="search"
+      />
+      <div class="banner">
+        <img class="banner__img" :src="bannerUrl" alt="" />
+      </div>
+      <div>hello</div>
+    </div>
+    <div class="docker">
+      <span
+        v-for="(item, idx) in dockerItems"
+        :key="idx"
+        class="docker__item"
+        :class="[item.isActive ? 'docker__item--active' : '']"
+      >
+        <i :class="[item.iconName]"></i>
+        <span>{{ item.label }}</span>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -21,8 +40,13 @@ export default {
       { iconName: "el-icon-user", label: "用户" },
       { iconName: "el-icon-setting", label: "设置" },
     ];
+    const searchInput = "";
+    const bannerUrl =
+      "https://png.pngtree.com/thumb_back/fw800/background/20190506/pngtree-simple-banner-fading-background-image_102822.jpg";
     return {
       dockerItems,
+      searchInput,
+      bannerUrl,
     };
   },
 };
@@ -31,6 +55,37 @@ export default {
 
 <style lang="scss">
 @import "./styles/config.scss";
+@import "./styles/mixins.scss";
+.wrapper {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0.5rem;
+  padding: 0 0.18rem;
+  .location {
+    @include flexCenterAll();
+    padding: 0.15rem 0;
+    font-size: 0.16rem;
+    &__icon {
+      margin-top: 0.05rem;
+      font-size: 0.25rem;
+      margin-right: 0.1rem;
+      line-height: 0.22rem;
+    }
+    span {
+      display: inline-block;
+      flex: 1;
+    }
+  }
+  .banner {
+    background-color: pink;
+    min-height: 1rem;
+    &__img {
+      width: 100%;
+    }
+  }
+}
 .docker {
   position: absolute;
   display: flex;
@@ -39,7 +94,7 @@ export default {
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top: 1px solid #000;
+  border-top: 0.01rem solid #000;
   align-items: center;
   justify-content: space-evenly;
   // & == .docker: 所以这边可以代替
