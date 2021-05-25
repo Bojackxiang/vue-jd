883e69d9726850a2d9839026895656c12c6b470b
Alex <alex@q-83.com>
2021-05-25 20:00:57 +1000

    day 1 | 更新了底部docker 的样式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="docker">
-    <span class="docker__item">test</span>
-    <span class="docker__item">test</span>
-    <span class="docker__item">test</span>
-    <span class="docker__item">test</span>
+    <span class="docker__item">
+      <i class="el-icon-s-home"> </i>
+      <span>首页</span>
+    </span>
+    <span class="docker__item">
+      <i class="el-icon-shopping-cart-2"></i>
+      <span>购物车</span>
+    </span>
+    <span class="docker__item">
+      <i class="el-icon-user"></i>
+      <span>用户</span>
+    </span>
+    <span class="docker__item">
+      <i class="el-icon-setting docker__item"></i>
+      <span>设置</span>
+    </span>
   </div>
 </template>
 
@@ -23,5 +35,13 @@
 .docker__item {
   flex: 1;
   text-align: center;
+  i {
+    font-size: 0.25rem;
+  }
+  span {
+    display: block;
+    font-size: 0.1rem;
+    transform: scale(0.8, 0.8);
+  }
 }
 </style>
